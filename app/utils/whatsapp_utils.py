f5877e8ac53c8b2b6d50ce7b713f536f1169dc9f
juandrengifo <juandrengifo@github.com>
2024-01-17 15:04:42 -0500

    Implemented whatsapp meta client bot

---
+++ app/utils/whatsapp_utils.py
@@ -0,0 +1,146 @@
+import logging
+from flask import current_app, jsonify
+import json
+import requests
+
+from salesgpt.salesgptapi import SalesGPTAPI
+
+# from app.services.openai_service import generate_response
+import re
+
+
+# Global dictionary to store conversation histories
+conversation_histories = {}
+
+def log_http_response(response):
+    logging.info(f"Status: {response.status_code}")
+    logging.info(f"Content-type: {response.headers.get('content-type')}")
+    logging.info(f"Body: {response.text}")
+
+
+def get_text_message_input(recipient, text):
+    return json.dumps(
+        {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": recipient,
+            "type": "text",
+            "text": {"preview_url": False, "body": text},
+        }
+    )
+
+
+
+def generate_response(sender, incoming_msg, conversation_history):
+    '''
+    Generates a response to a given message using SalesGPTAPI.
+    Maintains context using conversation histories.
+
+    Parameters:
+    sender (str): The ID of the sender (user).
+    incoming_msg (str): The incoming message from the user.
+    conversation_histories (dict): A dictionary maintaining conversation histories keyed by sender.
+
+    Returns:
+    str: The generated response message.
+    '''
+
+    # Initialize SalesGPTAPI
+    sales_api = SalesGPTAPI(config_path="")  # Assuming SalesGPTAPI can handle an empty config_path
+
+    # Generate a response using SalesGPTAPI
+    response = sales_api.do(conversation_history, incoming_msg)
+    name, reply = response["name"], response["reply"]
+
+    # Update the conversation history
+    conversation_history.append(f"User: {incoming_msg}")
+    conversation_history.append(f"{name}: {reply}")
+    conversation_histories[sender] = conversation_history
+
+
+
+    # Return the generated response
+    return reply[11:-13]
+
+
+
+def send_message(data):
+    headers = {
+        "Content-type": "application/json",
+        "Authorization": f"Bearer {current_app.config['ACCESS_TOKEN']}",
+    }
+
+    url = f"https://graph.facebook.com/{current_app.config['VERSION']}/{current_app.config['PHONE_NUMBER_ID']}/messages"
+
+    try:
+        response = requests.post(
+            url, data=data, headers=headers, timeout=10
+        )  # 10 seconds timeout as an example
+        response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
+    except requests.Timeout:
+        logging.error("Timeout occurred while sending message")
+        return jsonify({"status": "error", "message": "Request timed out"}), 408
+    except (
+        requests.RequestException
+    ) as e:  # This will catch any general request exception
+        logging.error(f"Request failed due to: {e}")
+        return jsonify({"status": "error", "message": "Failed to send message"}), 500
+    else:
+        # Process the response as normal
+        log_http_response(response)
+        return response
+
+
+def process_text_for_whatsapp(text):
+    # Remove brackets
+    pattern = r"\【.*?\】"
+    # Substitute the pattern with an empty string
+    text = re.sub(pattern, "", text).strip()
+
+    # Pattern to find double asterisks including the word(s) in between
+    pattern = r"\*\*(.*?)\*\*"
+
+    # Replacement pattern with single asterisks
+    replacement = r"*\1*"
+
+    # Substitute occurrences of the pattern with the replacement
+    whatsapp_style_text = re.sub(pattern, replacement, text)
+
+    return whatsapp_style_text
+
+
+def process_whatsapp_message(body):
+    wa_id = body["entry"][0]["changes"][0]["value"]["contacts"][0]["wa_id"]
+    name = body["entry"][0]["changes"][0]["value"]["contacts"][0]["profile"]["name"]
+
+    message = body["entry"][0]["changes"][0]["value"]["messages"][0]
+    message_body = message["text"]["body"]
+
+    # Retrieve the conversation history for the user
+    conversation_history = conversation_histories.get(wa_id, [])
+
+    # Generate a response using the new function
+    response = generate_response(wa_id, message_body, conversation_history)
+
+    # Update the conversation history
+    conversation_history.append(f"User: {message_body}")
+    conversation_history.append(f"Bot: {response}")
+    conversation_histories[wa_id] = conversation_history
+
+    # Formatting response and sending message
+    data = get_text_message_input(current_app.config["RECIPIENT_WAID"], response)
+    send_message(data)
+
+
+def is_valid_whatsapp_message(body):
+    """
+    Check if the incoming webhook event has a valid WhatsApp message structure.
+    """
+    return (
+        body.get("object")
+        and body.get("entry")
+        and body["entry"][0].get("changes")
+        and body["entry"][0]["changes"][0].get("value")
+        and body["entry"][0]["changes"][0]["value"].get("messages")
+        and body["entry"][0]["changes"][0]["value"]["messages"][0]
+    )
--- run.py
+++ run.py
@@ -1,82 +1,10 @@
-import argparse
-import json
-import os
+import logging
 
-from dotenv import load_dotenv
-from langchain.chat_models import ChatLiteLLM
+from app import create_app
 
-from salesgpt.agents import SalesGPT
 
-load_dotenv()  # loads .env file
-
-# LangSmith settings section, set TRACING_V2 to "true" to enable it
-# or leave it as it is, if you don't need tracing (more info in README)
-os.environ["LANGCHAIN_TRACING_V2"] = "false"
-os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
-os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_SMITH_API_KEY")
-os.environ["LANGCHAIN_PROJECT"] = ""  # insert you project name here
+app = create_app()
 
 if __name__ == "__main__":
-    # Initialize argparse
-    parser = argparse.ArgumentParser(description="Description of your program")
-
-    # Add arguments
-    parser.add_argument(
-        "--config", type=str, help="Path to agent config file", default=""
-    )
-    parser.add_argument("--verbose", type=bool, help="Verbosity",
-                        default=False)
-    parser.add_argument(
-        "--max_num_turns",
-        type=int,
-        help="Maximum number of turns in the sales conversation",
-        default=10,
-    )
-
-    # Parse arguments
-    args = parser.parse_args()
-
-    # Access arguments
-    config_path = args.config
-    verbose = args.verbose
-    max_num_turns = args.max_num_turns
-
-    llm = ChatLiteLLM(temperature=0.2, model_name="gpt-3.5-turbo-instruct")
-
-    if config_path == "":
-        print("No agent config specified, using a standard config")
-        # keep boolean as string to be consistent with JSON configs.
-        USE_TOOLS = "True"
-        if USE_TOOLS == "True":
-            sales_agent = SalesGPT.from_llm(
-                llm,
-                use_tools=USE_TOOLS,
-                product_catalog="examples/sample_product_catalog.txt",
-                salesperson_name="Ted Lasso",
-                verbose=verbose,
-            )
-        else:
-            sales_agent = SalesGPT.from_llm(llm, verbose=verbose)
-    else:
-        with open(config_path, "r", encoding="UTF-8") as f:
-            config = json.load(f)
-        print(f"Agent config {config}")
-        sales_agent = SalesGPT.from_llm(llm, verbose=verbose, **config)
-
-    sales_agent.seed_agent()
-    print("=" * 10)
-    cnt = 0
-    while cnt != max_num_turns:
-        cnt += 1
-        if cnt == max_num_turns:
-            print("Maximum number of turns reached - ending the conversation.")
-            break
-        sales_agent.step()
-
-        # end conversation
-        if "<END_OF_CALL>" in sales_agent.conversation_history[-1]:
-            print("Sales Agent determined it is time to end the conversation.")
-            break
-        human_input = input("Your response: ")
-        sales_agent.human_step(human_input)
-        print("=" * 10)
+    logging.info("Flask app started")
+    app.run(host="0.0.0.0", port=8000)
